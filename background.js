8b452ea9a70364aeb8a879bd09cff1b3c1c5a4ba
Shuhao Zhang <shuhaozhangchris@gmail.com>
2022-12-21 17:20:43 -0500

    implement all steps to authorize and get info of a user

--- background.js
+++ background.js
@@ -1,1 +1,41 @@
 import "./credentials.js";
+(async () => {
+  const client_id = await chrome.storage.session.get('client_id');
+  const client_secret = await chrome.storage.session.get('client_secret');
+
+  chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
+    if (request.AuthorizeUrl) {
+      const code = request.AuthorizeUrl.match(/\?code=([\w\/\-]+)/)[1];
+      const state = request.AuthorizeUrl.match(/\&state=([\w\/\-]+)/)[1];
+      if (state == "success") {
+        try {
+          const AuthTokenResponse = await fetch("https://github.com/login/oauth/access_token", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+              "client_id": client_id.client_id,
+              "client_secret": client_secret.client_secret,
+              "code": code,
+            })
+          });
+  
+          const AuthTokenData = await AuthTokenResponse.json();
+          const AuthToken = AuthTokenData.access_token;
+          const UserInfoResposne = await fetch("https://api.github.com/user", {
+            headers: {
+              "Authorization": `Bearer ${AuthToken}`
+            }
+          });
+
+          const UserData = await UserInfoResposne.json();
+          console.log(UserData);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  })
+})();
--- popup.js
+++ popup.js
@@ -1,34 +1,20 @@
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
-  console.log("sign in!!!");
   const params = new URLSearchParams();
   const client_id = await chrome.storage.session.get('client_id');
-  const client_secret = await chrome.storage.session.get('client_secret');
-  // chrome.storage.sync.get(['client_id'], function(result) {
-  //   console.log('Client_id retrieved: ' + result.key);
-  // });
-  // chrome.storage.sync.get(['client_secret'], function(result) {
-  //   console.log('Client_secret retrieved: ' + result.key);
-  // });
   params.append("client_id", client_id.client_id);
-  params.append("login", "ShuhaoZQGG");
   params.append("state", "success");
+  params.append("redirect_url", "htttps://github.com");
   console.log(params.toString());
-  const AuthorizeResponse = await fetch(`https://github.com/login/oauth/authorize?${params.toString()}&redirect_uri=https://github.com`, {
-    method: "GET",
-    redirect: "follow"
-  })
-  const code = AuthorizeResponse.url.split("?")[1].split("&")[0].split("=")[1];
-  const state = AuthorizeResponse.url.split("?")[1].split("&")[1].split("=")[1];
-  console.log(code);
-  console.log(state);
+  try {
+    const AuthorizeResponse = await fetch(`https://github.com/login/oauth/authorize?${params.toString()}`, {
+      method: "GET",
+      redirect: "follow"
+    })
+  
+    await chrome.runtime.sendMessage({ "AuthorizeUrl": AuthorizeResponse.url });
+  } catch (error) {
+    console.error(error);
+  }
 })
 
-const client_id = document.getElementById("client_id");
-const client_secret = document.getElementById("client_secret");
-client_id.addEventListener("input", function() {
-  console.log(this.value);
-});
-client_secret.addEventListener("input", function() {
-  console.log(this.value);
-});
