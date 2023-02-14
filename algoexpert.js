094886ff053569251eeec3c32fd37d411beb82f2
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-02-14 00:21:24 -0500

    minor changes

--- algoexpert.js
+++ algoexpert.js
@@ -29,92 +29,97 @@
   const solutionDivClass = "_5Y_y5dN7pkjcKB22vQiZ"
   const codeClass = "cm-content"
   const titleClass = "wBpuKvBGWdd7o3KaUFOQ";
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // Get all button elements
-    setTimeout(() => {
-      if (request.type="currentTab") {
-        Question = "";
-        Code = "";
-        QuestionTitle = "";
-        Question = "";
-        const title = document.getElementsByClassName(titleClass)[0].innerText;
-        const buttons = document.getElementsByTagName('button');
-        const questionDiv = document.getElementsByClassName(questionClass);
-        const questionDivChildNodes = questionDiv[0].childNodes;
-        const solutionDiv = document.getElementsByClassName(solutionDivClass)
-        const codeDiv = document.getElementsByClassName(codeClass);
-        SolutionDiv = solutionDiv;
-        CodeDiv = codeDiv;
-        QuestionTitle = title;
-        Question += "# " + QuestionTitle + '\n';
-        for (const [key, value] of Object.entries(questionDivChildNodes)) {
-          if (value.innerText && (value.innerText.includes("Sample Input") || value.innerText.includes("Sample Output"))) {
-            Question += "**" + value.innerText + "**"
-          } else if (value.textContent) {
-            Question += value.textContent;
-          } else if (value.innerText) {
-            Question += value.innerText
-          } 
-        }
-        // Iterate through the buttons
-        for (const button of buttons) {
-          // Get the span element inside the button
-          const span = button.querySelector('span');
-          // Check the content of the span element
-          if (span && span.textContent === 'Submit Code') {
-            // Do something with the button element
-            SubmitButton = button;
-            break;
+  try {
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      // Get all button elements
+      setTimeout(() => {
+        if (request.type="currentTab") {
+          Question = "";
+          Code = "";
+          QuestionTitle = "";
+          Question = "";
+          const title = document.getElementsByClassName(titleClass)[0].innerText;
+          const buttons = document.getElementsByTagName('button');
+          const questionDiv = document.getElementsByClassName(questionClass);
+          const questionDivChildNodes = questionDiv[0].childNodes;
+          const solutionDiv = document.getElementsByClassName(solutionDivClass)
+          const codeDiv = document.getElementsByClassName(codeClass);
+          SolutionDiv = solutionDiv;
+          CodeDiv = codeDiv;
+          QuestionTitle = title;
+          Question += "# " + QuestionTitle + '\n';
+          for (const [key, value] of Object.entries(questionDivChildNodes)) {
+            if (value.innerText && (value.innerText.includes("Sample Input") || value.innerText.includes("Sample Output"))) {
+              Question += "**" + value.innerText + "**"
+            } else if (value.textContent) {
+              Question += value.textContent;
+            } else if (value.innerText) {
+              Question += value.innerText
+            } 
           }
-        }
-      }
-      SubmitButton.addEventListener('click', async function() {
-        Code = CodeDiv[CodeDiv.length-1].innerText;
-
-        for (const [key, value] of Object.entries(SolutionDiv[0].childNodes)) {
-          if(value.classList.length == 2) {
-            SolutionButton = value.innerText;
-          }
-        }
-        const languageClass = "oWoHqZGV1RWlIC1vbJQA";
-        const resultClassName = "jq63ZT06FaZykTzWRxJS";
-        setTimeout(() => {
-          const language = document.getElementsByClassName(languageClass);
-          Language = language[0].innerText;
-          Extension = LanguageMapping[Language];
-          const resultPara = document.getElementsByClassName(resultClassName);
-          if (resultPara) {
-            const resultText = resultPara[0].innerText;
-            if (resultText.includes("Congratulations!")) {
-                      // This function will be executed when the button is clicked
-              console.log('send request');
-              chrome.runtime.sendMessage({
-                contentScriptQuery: "create solution",
-                name: QuestionTitle,
-                authToken: AuthToken,
-                owner: owner,
-                repo: repo,
-                solutionNo: SolutionButton,
-                question: Question,
-                code: Code,
-                language: Language,
-                extension: Extension
-              });
-
-            } else {
-              console.log("solution is wrong, not sending anything");
+          // Iterate through the buttons
+          for (const button of buttons) {
+            // Get the span element inside the button
+            const span = button.querySelector('span');
+            // Check the content of the span element
+            if (span && span.textContent === 'Submit Code') {
+              // Do something with the button element
+              SubmitButton = button;
+              break;
             }
           }
-        }, 1000)
-      });
-    }, 1000)
-
-    /**
-     * @description return false to close the chrome.runtime.onMessage to close the channel to avoid the error
-     * @error Uncaught (in promise) Error: A listener indicated an asynchronous response by returning true, 
-     *        but the message channel closed before a response was received
-     */
-    return false;
-  });
+        }
+        SubmitButton.addEventListener('click', async function() {
+          Code = CodeDiv[CodeDiv.length-1].innerText;
+  
+          for (const [key, value] of Object.entries(SolutionDiv[0].childNodes)) {
+            if(value.classList.length == 2) {
+              SolutionButton = value.innerText;
+            }
+          }
+          const languageClass = "oWoHqZGV1RWlIC1vbJQA";
+          const resultClassName = "jq63ZT06FaZykTzWRxJS";
+          setTimeout(() => {
+            const language = document.getElementsByClassName(languageClass);
+            Language = language[0].innerText;
+            Extension = LanguageMapping[Language];
+            const resultPara = document.getElementsByClassName(resultClassName);
+            if (resultPara) {
+              const resultText = resultPara[0].innerText;
+              if (resultText.includes("Congratulations!")) {
+                        // This function will be executed when the button is clicked
+                console.log('send request');
+                chrome.runtime.sendMessage({
+                  contentScriptQuery: "create solution",
+                  name: QuestionTitle,
+                  authToken: AuthToken,
+                  owner: owner,
+                  repo: repo,
+                  solutionNo: SolutionButton,
+                  question: Question,
+                  code: Code,
+                  language: Language,
+                  extension: Extension
+                });
+  
+              } else {
+                console.log("solution is wrong, not sending anything");
+              }
+            }
+          }, 1000)
+        });
+      }, 1000)
+  
+      /**
+       * @description return false to close the chrome.runtime.onMessage to close the channel to avoid the error
+       * @error Uncaught (in promise) Error: A listener indicated an asynchronous response by returning true, 
+       *        but the message channel closed before a response was received
+       */
+      return false;
+    });
+  } catch (error) {
+    console.log(error);
+    window.location.reload();
+  }
 })();
 
--- background.js
+++ background.js
@@ -6,7 +6,7 @@ import { createAuthToken, getUserInfo, getContent, createContent, updateContent 
   chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
     // Check if the tab's URL has changed
     if (
-      changeInfo.status === "complete"
+      changeInfo.url
     ) {
      if (tab.url && tab.url.match(/\?code=([\w\/\-]+)/)){
         const code = tab.url.match(/\?code=([\w\/\-]+)/)[1];
@@ -36,52 +36,56 @@ import { createAuthToken, getUserInfo, getContent, createContent, updateContent 
   let queryOptions = { active: true, lastFocusedWindow: true };
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   console.log(await chrome.tabs.query(queryOptions));
-  chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
-    if (request.Authorization_URL) {
-      chrome.tabs.create({url: request.Authorization_URL});
-    }
-
-    if (request.contentScriptQuery === "create solution") {
-      console.log("receive create solution Instruction");
-      try {
-        /**
-         *  @todo get file path: if readme is already created: skip it
-         *  @todo get file path: if any solution is already existed, get the sha number and update the content
-         * */ 
-                
-        const createReadMeMessage = "create the folder for the question and write question information into a README.md";
-        const { contentScriptQuery, name, authToken, owner, repo, solutionNo, question, code, language, extension } = request;
-        console.log("AuthToken received", authToken);
-        const getReadMeResponse = await getContent(owner, repo, `${name}/README.md`, authToken);
-        console.log('getReadMeResponse', getReadMeResponse);
-        if (getReadMeResponse.ok != true) {
-          const createReadMeResponse = await createContent(owner, repo, `${name}/README.md`, authToken, question, createReadMeMessage);
-          console.log("createReadMeResponse", createReadMeResponse);
-        } else {}
-
-        const createSolutionMessage = `Create ${language} ${solutionNo} for question ${name}`;
-        const changeSolutionMessage = `Change ${language} ${solutionNo} for question ${name}`;
-        const getSolutionResponse = await getContent(owner, repo, `${name}/${solutionNo}/${name}${extension}`, authToken)
-        const solutionData = await getSolutionResponse.json();
-        const sha = solutionData.sha;
-        console.log('sha', sha);
-        if (getReadMeResponse.ok == true) {
-          const changeSolutionResponse = await updateContent(owner, repo, `${name}/${solutionNo}/${name}${extension}`, sha, authToken, code, changeSolutionMessage);
-          console.log("changeSolutionResponse", changeSolutionResponse);
-        } else {
-          const createSolutionResponse = await createContent(owner, repo, `${name}/${solutionNo}/${name}${extension}`, authToken, code, createSolutionMessage); 
-          console.log("createSolutionResponse", createSolutionResponse);
-        }
-      } catch(error) {
-        console.log(error);
-      }    
-    }
-    
-    /**
-     * @description return false to close the chrome.runtime.onMessage to close the channel to avoid the error
-     * @error Uncaught (in promise) Error: A listener indicated an asynchronous response by returning true, 
-     *        but the message channel closed before a response was received
-     */
-    return false;
-  });  
+  try {
+    chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
+      if (request.Authorization_URL) {
+        chrome.tabs.create({url: request.Authorization_URL});
+      }
+  
+      if (request.contentScriptQuery === "create solution") {
+        console.log("receive create solution Instruction");
+        try {
+          /**
+           *  @todo get file path: if readme is already created: skip it
+           *  @todo get file path: if any solution is already existed, get the sha number and update the content
+           * */ 
+                  
+          const createReadMeMessage = "create the folder for the question and write question information into a README.md";
+          const { contentScriptQuery, name, authToken, owner, repo, solutionNo, question, code, language, extension } = request;
+          console.log("AuthToken received", authToken);
+          const getReadMeResponse = await getContent(owner, repo, `${name}/README.md`, authToken);
+          console.log('getReadMeResponse', getReadMeResponse);
+          if (getReadMeResponse.ok != true) {
+            const createReadMeResponse = await createContent(owner, repo, `${name}/README.md`, authToken, question, createReadMeMessage);
+            console.log("createReadMeResponse", createReadMeResponse);
+          } else {}
+  
+          const createSolutionMessage = `Create ${language} ${solutionNo} for question ${name}`;
+          const changeSolutionMessage = `Change ${language} ${solutionNo} for question ${name}`;
+          const getSolutionResponse = await getContent(owner, repo, `${name}/${solutionNo}/${name}${extension}`, authToken)
+          const solutionData = await getSolutionResponse.json();
+          const sha = solutionData.sha;
+          console.log('sha', sha);
+          if (getReadMeResponse.ok == true) {
+            const changeSolutionResponse = await updateContent(owner, repo, `${name}/${solutionNo}/${name}${extension}`, sha, authToken, code, changeSolutionMessage);
+            console.log("changeSolutionResponse", changeSolutionResponse);
+          } else {
+            const createSolutionResponse = await createContent(owner, repo, `${name}/${solutionNo}/${name}${extension}`, authToken, code, createSolutionMessage); 
+            console.log("createSolutionResponse", createSolutionResponse);
+          }
+        } catch(error) {
+          console.log(error);
+        }    
+      }
+      
+      /**
+       * @description return false to close the chrome.runtime.onMessage to close the channel to avoid the error
+       * @error Uncaught (in promise) Error: A listener indicated an asynchronous response by returning true, 
+       *        but the message channel closed before a response was received
+       */
+      return false;
+    });  
+  } catch (error) {
+    console.log("error", error);
+  }
 })();
