c0aa30170b1a7cc2ccc8cf49ca540469a74fd794
Shuhao Zhang <shuhaozhangchris@gmail.com>
2023-03-25 00:32:27 -0400

    move github calls from background to content script, avoid workers go sleep. Update/Create Scratchpad, comment out console.logs

--- algoexpert.js
+++ algoexpert.js
@@ -1,4 +1,6 @@
 (async () => {
+  const githubApiCalls = chrome.runtime.getURL('./githubApiCalls.js');
+  const githubApiCallsFunctions = await import(githubApiCalls);
   const LanguageMapping= {
     "Python": ".py",
     "JavaScript": ".js",
@@ -9,20 +11,29 @@
     "Kotlin": ".kts",
     "Switf": ".swift"
   }
-
+  const Owner = (await chrome.storage.local.get("username")).username;
+  const Repo = (await chrome.storage.local.get('repository')).repository;
+  const AuthToken = (await chrome.storage.local.get("auth_token")).auth_token;
+  //console.log("AuthToken", AuthToken);
+  //console.log("Owner", Owner);
+  //console.log("Repo", Repo);
   let SubmitButton = null;
   let QuestionTitle = "";
   let Question = "";
   let CodeDiv = null;
   let Code = "";
   let SolutionDiv = null;
-  let SolutionButton = null;
+  let SolutionNo = null;
   let Language = "";
   let Extension = "";
   const questionClass = "html";
   const solutionDivClass = "_5Y_y5dN7pkjcKB22vQiZ"
   const codeClass = "cm-content"
   const titleClass = "wBpuKvBGWdd7o3KaUFOQ";
+  // TODO Get scratchPad Content, publish it as a new file if it only has one child and the content !== Write whatever you want here.
+  const scratchPadClass = "cm-activeLine cm-line";
+  const scratchPadDiv = document.getElementsByClassName(scratchPadClass);
+  let scratchPadContent;
   let ErrorMessage = "";
   try {
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
@@ -42,7 +53,7 @@
           SolutionDiv = solutionDiv;
           CodeDiv = codeDiv;
           QuestionTitle = title;
-          Question += "# " + QuestionTitle + '\n';
+          Question += '<pre>' + `\n` + "# " + QuestionTitle + '\n';
           if (questionDivChildNodes) {
             for (const [key, value] of Object.entries(questionDivChildNodes)) {
               if (value.innerText && (value.innerText.includes("Sample Input") || value.innerText.includes("Sample Output"))) {
@@ -54,7 +65,7 @@
               } 
             }
           }
-
+          Question += '</pre>';
           // Iterate through the buttons
           for (const button of buttons) {
             // Get the span element inside the button
@@ -68,16 +79,28 @@
           }
         }
         SubmitButton?.addEventListener('click', async function() {
+          scratchPadContent = '<pre>'+ `\n` + scratchPadDiv[0]?.innerText + `\n`;
+          let scratchPadCurrentNode = scratchPadDiv[0];
+          // check if the next sibling of scratchPadDiv is a div with class cm-line, if yes, then continue to get the content of the next sibling until the next sibling is not a div with class cm-line
+          while (scratchPadCurrentNode?.nextSibling?.classList?.contains("cm-line")) {
+            scratchPadCurrentNode = scratchPadCurrentNode?.nextSibling;
+            // console.log(scratchPadCurrentNode);
+            scratchPadContent += scratchPadCurrentNode.innerText + `\n`;
+          }
+          scratchPadContent += '</pre>';
+          // console.log('scratchPadDiv', scratchPadDiv);
+          // console.log("scratchPadContent", scratchPadContent);
+
           Code = CodeDiv[CodeDiv.length-1].innerText;
   
           for (const [key, value] of Object.entries(SolutionDiv[0]?.childNodes)) {
             if(value.classList.length == 2) {
-              SolutionButton = value.innerText;
+              SolutionNo = value.innerText;
             }
           }
           const languageClass = "oWoHqZGV1RWlIC1vbJQA";
           const resultClassName = "jq63ZT06FaZykTzWRxJS";
-          setTimeout(() => {
+          setTimeout(async () => {
             const language = document?.getElementsByClassName(languageClass);
             Language = language[0]?.innerText;
             Extension = LanguageMapping[Language];
@@ -86,44 +109,70 @@
               const resultText = resultPara[0]?.innerText;
               if (resultText?.includes("Congratulations!")) {
                 // This function will be executed when the button is clicked
-                if (chrome.runtime?.id) {
-                  chrome.runtime.sendMessage({
-                    contentScriptQuery: "create solution",
-                    name: QuestionTitle,
-                    solutionNo: SolutionButton,
-                    question: Question,
-                    code: Code,
-                    language: Language,
-                    extension: Extension
-                  }, function (response) {
-                    if (response) {
-                      console.log(response);
-                    }
-                    if (chrome.runtime.lastError && ErrorMessage != chrome.runtime.lastError.message) {
-                      console.log('Error: ' + chrome.runtime.lastError.message);
-                      ErrorMessage = chrome.runtime.lastError.message;
-                    }
-                  });
+                  //console.log("AuthToken received", AuthToken);
+                  const getReadMeResponse = await githubApiCallsFunctions.getContent(Owner, Repo, `${QuestionTitle}/README.md`, AuthToken);
+                  if (getReadMeResponse.ok != true) {
+                    const createReadMeMessage = "create the folder for the question and write question information into a README.md";
+                    await githubApiCallsFunctions.createContent(Owner, Repo, `${QuestionTitle}/README.md`, AuthToken, Question, createReadMeMessage);
+                    // const createReadMeResponse = await githubApiCallsFunctions.createContent(Owner, Repo, `${QuestionTitle}/README.md`, AuthToken, Question, createReadMeMessage);
+                    // console.log("createReadMeResponse", createReadMeResponse);
+                  } else {
+                    const getReadMeData = await getReadMeResponse.json();
+                    const readMeSha = getReadMeData.sha;
+                    const updateReadMeMessage = "update the README.md with the question information";
+                    await githubApiCallsFunctions.updateContent(Owner, Repo, `${QuestionTitle}/README.md`, readMeSha, AuthToken, Question, updateReadMeMessage);
+                    // const updateReadMeResponse = await githubApiCallsFunctions.updateContent(Owner, Repo, `${QuestionTitle}/README.md`, readMeSha, AuthToken, Question, updateReadMeMessage);
+                    // console.log("updateReadMeResponse", updateReadMeResponse);
+                  }
+                  
 
-                  sendResponse("ok");
-                }
+                  const getScratchPadResponse = await githubApiCallsFunctions.getContent(Owner, Repo, `${QuestionTitle}/ScratchPad.md`, AuthToken);
+                  if (getScratchPadResponse.ok != true) {
+                    const createScratchPadMessage = `Create ScratchPad for question ${QuestionTitle}`;
+                    await githubApiCallsFunctions.createContent(Owner, Repo, `${QuestionTitle}/ScratchPad.md`, AuthToken, scratchPadContent, createScratchPadMessage);
+                  } else {
+                    const getScratchPadData = await getScratchPadResponse.json();
+                    const scratchPadSha = getScratchPadData.sha;
+                    const updateScratchPadMessage = `Update ScratchPad for question ${QuestionTitle}`;
+                    await githubApiCallsFunctions.updateContent(Owner, Repo, `${QuestionTitle}/ScratchPad.md`, scratchPadSha, AuthToken, scratchPadContent, updateScratchPadMessage);
+                  }
+
+
+
+                  const createSolutionMessage = `Create ${Language} ${SolutionNo} for question ${QuestionTitle}`;
+                  const changeSolutionMessage = `Change ${Language} ${SolutionNo} for question ${QuestionTitle}`;
+                  const getSolutionResponse = await githubApiCallsFunctions.getContent(Owner, Repo, `${QuestionTitle}/${SolutionNo}/${QuestionTitle}${Extension}`, AuthToken)
+                  const solutionData = await getSolutionResponse.json();
+                  const sha = solutionData.sha;
+                  if (getSolutionResponse.ok == true) {
+                    const changeSolutionResponse = await githubApiCallsFunctions.updateContent(Owner, Repo, `${QuestionTitle}/${SolutionNo}/${QuestionTitle}${Extension}`, sha, AuthToken, Code, changeSolutionMessage);
+                    if (changeSolutionResponse.status != 200 && changeSolutionResponse.status != 201) {
+                      throw new Erwror("create solution failed");
+                    } else {
+                      // console.log("changeSolutionResponse", changeSolutionResponse);
+                    }          
+                  } else {
+                    const createSolutionResponse = await githubApiCallsFunctions.createContent(Owner, Repo, `${QuestionTitle}/${SolutionNo}/${QuestionTitle}${Extension}`, AuthToken, Code, createSolutionMessage); 
+                    if (createSolutionResponse.status != 200 && createSolutionResponse.status != 201) {
+                      throw new Error("create solution failed");
+                    } else {
+                      // console.log("createSolutionResponse", createSolutionResponse);
+                    }
+                  }}
               } else {
-                sendResponse("error");
                 console.log("solution is wrong, not sending anything");
               }
-            }
-            return true;
-          }, 1000)
+            }, 1000)
         });
-        return true;
+        return false;
       }, 1000)
 
       sendResponse("finished");
-      return true;
+      return false;
     });
   } catch (error) {
     sendResponse("error");
-    console.log(error);
+    //console.log(error);
   }
 })();
 
